0644833d2ff1dd1e871dee975b484f9798e61a1c
scisjg <james@jggillespie.co.uk>
2016-02-17 01:56:37 +0000

    test

---
+++ build2015-weather-station-task/StartupTask.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Windows.ApplicationModel.Background;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+using Windows.System.Threading;
+
+using Microsoft.Maker.Sparkfun.WeatherShield;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace build2015_weather_station_task
+{
+    public sealed partial class StartupTask : IBackgroundTask
+    {
+        private readonly int i2cReadIntervalSeconds = 2;
+        private ThreadPoolTimer i2cTimer;
+        private Mutex mutex;
+        private string mutexId = "WeatherStation";
+        private readonly int port = 50001;
+        private ThreadPoolTimer SasTokenRenewTimer;
+        private HttpServer server;
+        private WeatherShield shield = new WeatherShield("I2C1", 6, 5);
+        private BackgroundTaskDeferral taskDeferral;
+        private WeatherData weatherData = new WeatherData();
+
+        // Hard coding guid for sensors. Not an issue for this particular application which is meant for testing and demos
+        private List<ConnectTheDotsSensor> sensors = new List<ConnectTheDotsSensor> {
+            //Format for a new sensor is as follows:
+            //new ConnectTheDotsSensor("YOUR_GUID_HERE", "VALUE_DESCRIPTOR", "UNIT_OF_MEASUREMENT");
+            new ConnectTheDotsSensor("2298a348-e2f9-4438-ab23-82a3930662ab", "Altitude", "m"),
+            new ConnectTheDotsSensor("2298a348-e2f9-4438-ab23-82a3930662ac", "Humidity", "%RH"),
+            new ConnectTheDotsSensor("2298a348-e2f9-4438-ab23-82a3930662ad", "Pressure", "kPa"),
+            new ConnectTheDotsSensor("2298a348-e2f9-4438-ab23-82a3930662ae", "Temperature", "C"),
+        };
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Ensure our background task remains running
+            taskDeferral = taskInstance.GetDeferral();
+
+            // Mutex will be used to ensure only one thread at a time is talking to the shield / isolated storage
+            mutex = new Mutex(false, mutexId);
+         
+            // Initialize ConnectTheDots Settings
+            localSettings.ServicebusNamespace = "windowactuator-ns";
+            localSettings.EventHubName = "ehdevices";
+            localSettings.KeyName = "D1";
+            localSettings.Key = "1uMOwjURpgGX9l5JqnYeatBkIRoLzP7qH8YGFUeAIrU=";
+            localSettings.DisplayName = GetHostName();
+            localSettings.Organization = "Ulster University";
+            localSettings.Location = "North Europe";
+
+            SaveSettings();
+
+            // Initialize WeatherShield
+            await shield.BeginAsync();
+
+            // Create a timer-initiated ThreadPool task to read data from I2C
+            i2cTimer = ThreadPoolTimer.CreatePeriodicTimer(PopulateWeatherData, TimeSpan.FromSeconds(i2cReadIntervalSeconds));
+
+            // Start the server
+            server = new HttpServer(port);
+            var asyncAction = ThreadPool.RunAsync((w) => { server.StartServer(shield, weatherData); });
+
+            // Task cancellation handler, release our deferral there 
+            taskInstance.Canceled += OnCanceled;
+
+            // Create a timer-initiated ThreadPool task to renew SAS token regularly
+            SasTokenRenewTimer = ThreadPoolTimer.CreatePeriodicTimer(RenewSasToken, TimeSpan.FromMinutes(15));
+        }
+
+        private string GetHostName()
+        {
+            foreach (HostName name in NetworkInformation.GetHostNames())
+            {
+                if (HostNameType.DomainName == name.Type)
+                {
+                    return name.DisplayName;
+                }
+            }
+
+            return "minwinpc";
+        }
+
+        private void OnCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            // Relinquish our task deferral
+            taskDeferral.Complete();
+        }
+
+        private void PopulateWeatherData(ThreadPoolTimer timer)
+        {
+            bool hasMutex = false;
+
+            try
+            {
+                hasMutex = mutex.WaitOne(1000);
+                if (hasMutex)
+                {
+                    weatherData.TimeStamp = DateTime.Now.ToLocalTime().ToString();
+
+                    shield.BlueLedPin.Write(Windows.Devices.Gpio.GpioPinValue.High);
+
+                    weatherData.Altitude = shield.Altitude;
+                    weatherData.BarometricPressure = shield.Pressure;
+                    weatherData.CelsiusTemperature = shield.Temperature;
+                    weatherData.FahrenheitTemperature = (weatherData.CelsiusTemperature * 9 / 5) + 32;
+                    weatherData.Humidity = shield.Humidity;
+
+                    shield.BlueLedPin.Write(Windows.Devices.Gpio.GpioPinValue.Low);
+
+                    // Push the WeatherData cloud storage (viewable at http://iotbuildlab.azurewebsites.net/)
+                    SendDataToConnectTheDots();
+                }
+            }
+            finally
+            {
+                if (hasMutex)
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+
+        private void RenewSasToken(ThreadPoolTimer timer)
+        {
+            bool hasMutex = false;
+
+            try
+            {
+                hasMutex = mutex.WaitOne(1000);
+                if (hasMutex)
+                {
+                    UpdateSasToken();
+                }
+            }
+            finally
+            {
+                if (hasMutex)
+                {
+                    mutex.ReleaseMutex();
+                }
+            }
+        }
+
+        private void SendDataToConnectTheDots()
+        {
+            ConnectTheDotsSensor sensor;
+            string time = DateTime.UtcNow.ToString("o");
+
+            // Send the altitude data
+            sensor = sensors.Find(item => item.measurename == "Altitude");
+            if (sensor != null)
+            {
+                sensor.value = weatherData.Altitude;
+                sensor.timecreated = time;
+                sendMessage(sensor.ToJson());
+            }
+
+            // Send the humidity data
+            sensor = sensors.Find(item => item.measurename == "Humidity");
+            if (sensor != null)
+            {
+                sensor.value = weatherData.Humidity;
+                sensor.timecreated = time;
+                sendMessage(sensor.ToJson());
+            }
+
+            // Sending the pressure data
+            sensor = sensors.Find(item => item.measurename == "Pressure");
+            if (sensor != null)
+            {
+                sensor.value = (weatherData.BarometricPressure / 1000);
+                sensor.timecreated = time;
+                sendMessage(sensor.ToJson());
+            }
+
+            // Sending the temperature data
+            sensor = sensors.Find(item => item.measurename == "Temperature");
+            if (sensor != null)
+            {
+                sensor.value = weatherData.CelsiusTemperature;
+                sensor.timecreated = time;
+                sendMessage(sensor.ToJson());
+            }
+        }
+    }
+}
---
+++ build2015-weather-station/MainPage.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.System.Threading;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.Web.Http;
+using Windows.Web.Http.Filters;
+
+using build2015_weather_station_task;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace build2015_weather_station
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private WeatherData data = new WeatherData();
+        private HttpClient weatherClient;
+        private HttpBaseProtocolFilter weatherFilter = new HttpBaseProtocolFilter();
+        //TODO: On the following line, replace "minwinpc" with the computer name of your IoT device (i.e. "http://<iot_device_name>:50001").
+        private Uri weatherUri = new Uri("http://192.168.0.14:50001");
+
+        public MainPage()
+        {
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+            InitializeComponent();
+
+            // Setup client to read from weather server
+            weatherFilter.CacheControl.ReadBehavior = HttpCacheReadBehavior.MostRecent;
+            weatherClient = new HttpClient(weatherFilter);
+
+            LogToScreen("Attempting to read from endpoint: " + weatherUri);
+            LogToScreen("");
+
+            // Create a timer-initiated ThreadPool task to read data from I2C
+            ThreadPoolTimer readerTimer = ThreadPoolTimer.CreatePeriodicTimer(async (source) =>
+            {
+                await ClearLogScreen();
+                QueryWeatherData();
+
+                // Notify the UI to do an update.
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Low, () => UpdateScreen());
+
+            }, TimeSpan.FromSeconds(2));
+
+        }
+
+        private async Task ClearLogScreen(CoreDispatcherPriority priority = CoreDispatcherPriority.Low)
+        {
+                await Dispatcher.RunAsync(priority, () => { Status.Text = ""; });
+        }
+
+        private async void LogToScreen (string text, CoreDispatcherPriority priority = CoreDispatcherPriority.Low)
+        {
+            await Dispatcher.RunAsync(priority, () => { Status.Text += text + "\n"; });
+        }
+
+        async void QueryWeatherData()
+        {
+            // Query weather data
+            try {
+                using (HttpResponseMessage response = await weatherClient.GetAsync(weatherUri))
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string responseString = await response.Content.ReadAsStringAsync();
+
+                        // Parse JSON response
+                        JsonObject jWeatherData = JsonObject.Parse(responseString);
+                        JsonValue jAltitude = jWeatherData.GetNamedValue("Altitude");
+                        JsonValue jBarometricPressure = jWeatherData.GetNamedValue("BarometricPressure");
+                        JsonValue jCelsiusTemperature = jWeatherData.GetNamedValue("CelsiusTemperature");
+                        JsonValue jFahrenheitTemperature = jWeatherData.GetNamedValue("FahrenheitTemperature");
+                        JsonValue jHumidity = jWeatherData.GetNamedValue("Humidity");
+                        JsonValue jTimeStamp = jWeatherData.GetNamedValue("TimeStamp");
+
+                        // Update screen with parsed data
+                        data.TimeStamp = jTimeStamp.GetString();
+                        LogToScreen("Parsed time stamp value: " + data.TimeStamp);
+
+                        data.Altitude = (float)jAltitude.GetNumber();
+                        LogToScreen("Parsed altitude value: " + data.Altitude);
+
+                        data.BarometricPressure = (float)jBarometricPressure.GetNumber();
+                        LogToScreen("Parsed barometric pressure value: " + data.BarometricPressure);
+
+                        data.CelsiusTemperature = (float)jCelsiusTemperature.GetNumber();
+                        LogToScreen("Parsed celsius temperature value: " + data.CelsiusTemperature);
+
+                        data.FahrenheitTemperature = (float)jFahrenheitTemperature.GetNumber();
+                        LogToScreen("Parsed Fahrenheit temperature value: " + data.FahrenheitTemperature);
+
+                        data.Humidity = (float)jHumidity.GetNumber();
+                        LogToScreen("Parsed humidity value: " + data.Humidity);
+                    }
+                    else
+                    {
+                        LogToScreen("ERROR: Unable to successfully reach " + weatherUri + "!");
+                        LogToScreen("Status Code: " + response.StatusCode);
+                        LogToScreen("Message: " + response.ReasonPhrase);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LogToScreen("ERROR: " + e.HResult + " {\r\n" + e.Message + "}");
+            }
+            finally
+            {
+                LogToScreen("");
+            }
+        }
+
+        private void Status_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            // Automatically scroll screen down, when new data is entered
+            var grid = (Grid)VisualTreeHelper.GetChild(Status, 0);
+            for (var i = 0; i <= VisualTreeHelper.GetChildrenCount(grid) - 1; i++)
+            {
+                object obj = VisualTreeHelper.GetChild(grid, i);
+                if (!(obj is ScrollViewer)) continue;
+                ((ScrollViewer)obj).ChangeView(0.0f, ((ScrollViewer)obj).ExtentHeight, 1.0f);
+                break;
+            }
+        }
+
+        private void UpdateScreen()
+        {
+            TimeStamp.Text = data.TimeStamp;
+
+            Altimeter.Value = data.Altitude;
+            Altitude.Text = string.Format("{0:N2}m", Altimeter.Value);
+
+            Hygrometer.Value = data.Humidity;
+            Humidity.Text = string.Format("{0:N2}%RH", Hygrometer.Value);
+
+            Barometer.Value = data.BarometricPressure / 1000;
+            Pressure.Text = string.Format("{0:N4}kPa", Barometer.Value);
+
+            Thermometer.Value = data.CelsiusTemperature;
+            Temperature.Text = string.Format("{0:N2}C", Thermometer.Value);
+        }
+    }
+}
